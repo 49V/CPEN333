76bcefc479d57750bd23fad9af08181b03b33ee4
49V <moshelawlor@gmail.com>
2017-09-28 18:16:08 -0700

    Question 2 Parallel Code
    This completes most of the functionality for the MonteCarlo integration
    for the parllel implementation. Classes should be added for the the
    different density functions

--- Question 2/montecarlo.cpp
+++ Question 2/montecarlo.cpp
@@ -26,7 +26,7 @@ double monteCarloIntegration::calculate_radius(double x, double y, double z){
 	return sqrt((x * x) + (y * y) + (z * z));
 }
 
-void monteCarloIntegration::calculate_center_of_mass(std::vector<double>& Cx, std::vector<double>& Cy, std::vector<double>& Cz, int index){
+void monteCarloIntegration::calculate_center_of_mass(std::vector<double>& Cx, std::vector<double>& Cy, std::vector<double>& Cz, int index, double numberOfSamples){
 		
 	const double volume = 8.0;
 	double xTest = 0.0;
@@ -35,7 +35,7 @@ void monteCarloIntegration::calculate_center_of_mass(std::vector<double>& Cx, st
 	double rhoSum = 0;
 	double density, rhoAverage;
 	
-	for(int i = 0; i < this->numberOfSamples; ++i){
+	for(int i = 0; i < numberOfSamples; ++i){
 		density = get_density(this->x[i], this->y[i], this->z[i]);
 		xTest += this->x[i] * density;
 		yTest += this->y[i] * density;
@@ -43,18 +43,63 @@ void monteCarloIntegration::calculate_center_of_mass(std::vector<double>& Cx, st
 		rhoSum += density;
 	}
 	
+	rhoAverage = (volume / numberOfSamples) * rhoSum;
 	
-	rhoAverage = (volume / this->numberOfSamples) * rhoSum;
-	
-	std::cout << "V: " << volume << " n: " << numberOfSamples << " p: " << rhoAverage << std::endl;
-	
-	Cx[index] = (volume / this->numberOfSamples) * xTest / rhoAverage;
-	Cy[index] = (volume / this->numberOfSamples) * yTest / rhoAverage;
-	Cz[index] = (volume / this->numberOfSamples) * zTest / rhoAverage;
+	Cx[index] = (volume / numberOfSamples) * xTest / rhoAverage;
+	Cy[index] = (volume / numberOfSamples) * yTest / rhoAverage;
+	Cz[index] = (volume / numberOfSamples) * zTest / rhoAverage;
 	
 	return;
 	
 }
+
+
+void monteCarloIntegration::parallel_calculate_center_of_mass(double& Cx, double& Cy, double& Cz, int numberOfSamples){
+
+	int numberOfThreads = std::thread::hardware_concurrency();
+	std::vector<double> partialCx (numberOfThreads, 0.0);
+	std::vector<double> partialCy (numberOfThreads, 0.0);
+	std::vector<double> partialCz (numberOfThreads, 0.0);
+	std::vector<std::thread> threads;
+	
+	
+	Cx = 0;
+	Cy = 0;
+	Cz = 0;
+	
+	
+	
+	
+	int mSamples = 0;
+	//Partition data and start threads
+	for(int i = 0; i < numberOfThreads - 1; ++i){
+		threads.push_back(std::thread(&monteCarloIntegration::calculate_center_of_mass, this, std::ref(partialCx), std::ref(partialCy), std::ref(partialCz), i, numberOfSamples / numberOfThreads));
+		mSamples += numberOfSamples / numberOfThreads;
+	}
+	
+	// Deal with remaining samples
+	threads.push_back(std::thread(&monteCarloIntegration::calculate_center_of_mass, this, std::ref(partialCx), std::ref(partialCy), std::ref(partialCz), (numberOfThreads - 1), (numberOfSamples - mSamples)));
+	
+	// Wait for the threads to finish
+	for(int i = 0; i < numberOfThreads; ++i){
+		threads[i].join();
+	}
+	
+	
+	// Add up all the values and come up with an average to return
+	for(int i = 0; i < numberOfThreads; ++i){
+		Cx += partialCx[i];
+		Cy += partialCy[i];
+		Cz += partialCz[i];
+	}
+	
+	Cx = Cx / numberOfThreads;
+	Cy = Cy / numberOfThreads;
+	Cz = Cz / numberOfThreads;
+	
+	return;
+}
+
 
 int main(){
 
@@ -62,15 +107,24 @@ int main(){
 	std::vector<double> Cx (1, 0);
 	std::vector<double> Cy (1, 0);
 	std::vector<double> Cz (1, 0);
+	double x, y, z;
 	
 	monteCarloIntegration test(numberOfSamples);
 	
-	
-	test.calculate_center_of_mass(Cx, Cy, Cz, 0);
-	
+	// Sequential Run
+	test.calculate_center_of_mass(Cx, Cy, Cz, 0, numberOfSamples);
+	std::cout << "Sequential Run: " << std::endl;
 	std::cout << "Cx: " << Cx[0] << std::endl;
 	std::cout << "Cy: " << Cy[0] << std::endl;
 	std::cout << "Cz: " << Cz[0] << std::endl;
-	
+	std::cout << "-------------------------------" << std::endl;
+
+	// Concurrent Run
+	test.parallel_calculate_center_of_mass(x, y, z, numberOfSamples);
+	std::cout << "Parallel Run: " << std::endl;
+	std::cout << "Cx: " << x << std::endl;
+	std::cout << "Cy: " << y << std::endl;
+	std::cout << "Cz: " << z << std::endl;
+
 	return 0;
 }
--- Question 2/montecarlo.h
+++ Question 2/montecarlo.h
@@ -7,20 +7,17 @@
 #include <random>
 #include <thread>
 #include <vector>
-#inlude "math.h"
+#include "math.h"
 
 class monteCarloIntegration{
 	
 	public:
 	
-	int numberOfSamples;
 	std::vector<double> x;
 	std::vector<double> y;
 	std::vector<double> z;
 	
 	monteCarloIntegration(int numberOfSamples){
-		
-		this->numberOfSamples = numberOfSamples;
 		
 		std::default_random_engine randomDistribution(std::chrono::system_clock::now().time_since_epoch().count());
 		std::uniform_real_distribution<double> distribution(-1.0, 1.0);
@@ -35,7 +32,8 @@ class monteCarloIntegration{
 	}
 	
 	virtual double get_density(double x, double y, double z);
-	void calculate_center_of_mass(std::vector<double>& Cx, std::vector<double>& Cy, std::vector<double>& Cz, int index);
+	void calculate_center_of_mass(std::vector<double>& Cx, std::vector<double>& Cy, std::vector<double>& Cz, int index, double numberOfSamples);
+	void parallel_calculate_center_of_mass(double& Cx, double& Cy, double& Cz, int index);
 	double calculate_radius(double x, double y, double z);
 	
 };
